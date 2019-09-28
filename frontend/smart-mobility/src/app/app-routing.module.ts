3aa587883e8af89b71f43362195abfe9b154c5de
Jonas Denis <jonas.denis@adesso.de>
2019-09-28 13:14:01 +0200

    added routing

--- frontend/smart-mobility/src/app/app-routing.module.ts
+++ frontend/smart-mobility/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchPageComponent} from './search-page/search-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchPageComponent},
+  { path: '', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppRoutingModule { }
